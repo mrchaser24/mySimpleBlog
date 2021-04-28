4e3b9e4a8894258f23245b5ee66568a8e7695a8f
mrchaser24 <chaserlovesyou@yahoo.com>
2021-04-28 20:51:47 +0800

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,10 @@
+<div class="footer">
+    <p>MyViewers &copy; <?php echo date('Y'); ?></p>
+</div>
+<!-- // footer -->
+
+</div>
+<!-- // container -->
+</body>
+
+</html>
---
+++ includes/head_section.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Averia+Serif+Libre|Noto+Serif|Tangerine" rel="stylesheet">
+    <!-- Styling for public area -->
+    <link rel="stylesheet" href="../src/css/public_style.css">
+    <meta charset="UTF-8">
